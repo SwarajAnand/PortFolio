8504f1eda4b60350db7f7d0c0a0d7a9a769ea6ff
Swaraj Anand <swrjnnd@gmail.com>
2023-02-09 22:30:11 +0530

    pending

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./css/header.css";
+import Mobile from "./Mobile";
+import Web from "./Web";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="header">
+      <div className="logo">SWARAJ ANAND </div>
+      <div className="menu">
+        <div className="web-menu">
+          <Web />
+        </div>
+        <div className="mobile-menu">
+          {/* <Mobile /> */}
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <i class="fi fi-rr-menu-burger menu-icon"></i>
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Web.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./css/web.css";
+import skillImage from "./settings.png";
+
+function Web() {
+  return (
+    <div className="web">
+      <div className="web-option">
+        <a href="#projects">
+          <i class="fi fi-rr-edit-alt option-icon"></i>Projects
+        </a>
+      </div>
+
+      <div className="web-option">
+        <a href="#skills">
+          <img
+            className="skillImage option-icon"
+            src={skillImage}
+            alt="skill"
+          ></img>
+          Skills
+        </a>
+      </div>
+
+      <div className="web-option">
+        <a href="#work">
+          <i class="fi fi-rr-briefcase option-icon"></i>Work
+        </a>
+      </div>
+
+      <div className="web-option">
+        <a href="#contact">
+          <i class="fi fi-rr-user option-icon"></i>Contact
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Web;
